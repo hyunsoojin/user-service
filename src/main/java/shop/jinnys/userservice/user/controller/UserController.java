332ae4c0775d7ce323b29dbdbdcc58063bd12caf
hyunsoojin <jhs7251@naver.com>
2025-10-17 15:01:48 +0900

    0.1.0 init

---
+++ src/main/java/shop/jinnys/userservice/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package shop.jinnys.userservice.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
